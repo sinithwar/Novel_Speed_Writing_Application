3da22b613ca3075de5cb1447899c1ac028974835
sinithwar <sinithwar@yahoo.com>
2016-12-02 14:34:57 -0500

    Added Word Count Activity and Finished Previous Activity's text and imaging

---
+++ app/src/main/java/tommymertell/novelspeedwriting/WordCountActivity.java
@@ -0,0 +1,13 @@
+package tommymertell.novelspeedwriting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordCountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_count);
+    }
+}
