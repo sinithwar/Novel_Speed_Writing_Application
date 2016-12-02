26c26cb9aac2dd227992439e1c01308cebd430db
sinithwar <sinithwar@yahoo.com>
2016-12-02 10:45:13 -0500

    Added Appropriate Views for Landing Page TextView Menu

---
+++ app/src/main/java/tommymertell/novelspeedwriting/PageSummaryActivity.java
@@ -0,0 +1,13 @@
+package tommymertell.novelspeedwriting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_summary);
+    }
+}
